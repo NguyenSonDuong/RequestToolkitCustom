629ce645f8dd5e2368309fdee2492584cf1235d5
NguyenSonDuong <hunterdemon9x99@gmail.com>
2021-06-13 17:52:28 +0700

    not change

--- RequestToolkit/model/DataRequest.cs
+++ RequestToolkit/model/DataRequest.cs
@@ -9,27 +9,43 @@ namespace RequestToolkit.model
 {
     public class DataRequest
     {
-        String url;
-        String header;
-        String body;
-        String responsive;
+        private String url;
+        private String headerRequest;
+        private String bodyRequest;
+        private String responsive;
+        private String headerResponsive;
         public String GetData(String regex,String type, int pos = 0)
         {
             if (type.Equals(RequestKey.HEADER))
             {
-                return Regex.Match(header, regex).Groups[pos].ToString();
+                return Regex.Match(headerResponsive, regex).Groups[pos].ToString();
             }else if (type.Equals(RequestKey.URL))
             {
-                return Regex.Match(url, regex).Groups[0].ToString();
+                return Regex.Match(url, regex).Groups[pos].ToString();
             }
             else if (type.Equals(RequestKey.RESPONSIVE))
             {
-                return Regex.Match(responsive, regex).Groups[0].ToString();
+                return Regex.Match(responsive, regex).Groups[pos].ToString();
             }
-            else if (type.Equals(RequestKey.BODY))
-            {
-                return Regex.Match(body, regex).Groups[0].ToString();
-            }
+            return null;
         }
+        public String SetData(String regex,String data ,String type, int pos = 0)
+        {
+            Regex regexObj = new Regex(regex);
+            if (type.Equals(RequestKey.HEADER))
+            {
+                return R
+            }
+            else if (type.Equals(RequestKey.URL))
+            {
+                return Regex.Match(url, regex).Groups[pos].ToString();
+            }
+            else if (type.Equals(RequestKey.RESPONSIVE))
+            {
+                return Regex.Match(responsive, regex).Groups[pos].ToString();
+            }
+            return null;
+        }
+
     }
 }
